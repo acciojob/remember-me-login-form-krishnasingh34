9c4cd92b328b3eaf6f04d8d007b585b0455742d1
Krishna Singh <144099384+krishnasingh34@users.noreply.github.com>
2025-03-17 23:28:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+let form = document.querySelector('form')
+let usernameBox = document.querySelector('#username')
+let passwordBox = document.querySelector('#password')
+let checkbox = document.querySelector('#checkbox')
+let btn = document.querySelector('#submit')
+let div = document.querySelector('.new')
+
+btn.addEventListener('click', (e)=>{
+    e.preventDefault()
+    let username = usernameBox.value
+    let password = passwordBox.value
+    alert(`Logged in as ${username}`)
+    checkbox.addEventListener('click', ()=>{
+        check = checkInput.checked
+        if(check){
+            localStorage.setItem('username', JSON.stringify(username))
+            localStorage.setItem('passowrd', JSON.stringify(password))
+            let newbtn = document.createElement('button')
+            newbtn.id = 'existing'
+            newbtn.innerText = 'Login as existing user'
+            div.append(newbtn)
+            newbtn.addEventListener('click',()=>{
+                alert(`Logged in as ${username}`)
+            })
+        }
+        else{
+            localStorage.removeItem('username')
+            localStorage.removeItem('passowrd')
+        }
+    })
+})
+
